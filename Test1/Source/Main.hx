180e053844dc3914cdc81ef5fcc11e8b47101c9d
Jean-Denis Boivin <jeandenis.boivin@gmail.com>
2017-04-19 14:13:50 -0400

    Testing

--- Test1/Source/Main.hx
+++ Test1/Source/Main.hx
@@ -8,19 +8,21 @@ import openfl.Assets;
 
 
 class Main extends Sprite {
-	
-	
+
+
 	public function new () {
-		
+
 		super ();
-		
+
+		trace("TEST!!");
+
 		var bitmap = new Bitmap (Assets.getBitmapData ("assets/openfl.png"));
 		addChild (bitmap);
-		
+
 		bitmap.x = (stage.stageWidth - bitmap.width) / 2;
 		bitmap.y = (stage.stageHeight - bitmap.height) / 2;
-		
+
 	}
-	
-	
+
+
 }
